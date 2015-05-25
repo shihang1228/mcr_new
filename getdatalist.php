fcd5f2019c85d6dee79115b18e7ea7ac2ecaf5e0
wuheng66888 <78057853@qq.com>
2015-05-25 15:09:48 +0800

    备份木材人

---
+++ getdatalist.php
@@ -0,0 +1,87 @@
+﻿<?php
+    include_once('db_fns.php');
+	header("Content-Type: text/html; charset=utf-8");
+	$conn = db_connect();
+	$filtervalue="";
+    $noconnect ="<p>对不起，没有查找到您要查找的内容！</p>";
+	$pagesize=30;
+	//读取参数
+    $num=$_POST['num'];
+	$areavalue =$_POST['areaselect'];
+	$kindvalue =$_POST['kindselect'];
+	$stuffvalue =$_POST['stuffselect'];
+	$productlen=$_POST['productlen'];
+	
+	$productwide=$_POST['productwide'];
+	$thinckness=$_POST['thinckness'];
+	
+	$diameterlen=$_POST['diameterlen'];
+	$timber=$_POST['timber'];
+	//判断条件
+	if($areavalue != 0){
+		$filtervalue =$filtervalue . " and p.portid =".$areavalue;
+	}
+	if($kindvalue !=0){
+		$filtervalue =$filtervalue ." and p.kindid =".$kindvalue;
+	}
+	if($stuffvalue !=0){
+	    $filtervalue =$filtervalue ." and p.stuffid =".$stuffvalue;
+	}
+	if($productlen !=0){
+	    $filtervalue =$filtervalue ." and p.productlen =".$productlen;
+	}
+	//依据是否是原木来增加条件
+	if($kindvalue ==1){//原木
+		if($diameterlen !=0) {
+		    $filtervalue =$filtervalue ." and p.diameterlen =".$diameterlen;
+		}
+		if($timber !=0) {
+		    $filtervalue =$filtervalue ." and p.timber =".$timber;
+		}
+	} else if( $kindvalue >1) {//其他类
+		if (($productwide !=0) &&($thinckness !=0)) {
+		    $filtervalue =$filtervalue ." and p.productwide =".$productwide  
+			." and p.thinckness = " .$thinckness ;
+		}
+	}
+	
+	
+	
+	 $query = "select productid,carnum,kindname,stuffname,phone,portname,updatetime,productlen,diameterlen,wide,thinckness   "
+           ." from t_product p,t_kind k,t_stuff s,t_port r,t_user u "
+          ." where p.userid = u.userid and p.kindid = k.kindid and "
+		  ." p.stuffid = s.stuffid and p.portid =r.portid ";	  
+		
+	if (strlen(trim($filtervalue))>0){
+		$query =$query . $filtervalue;
+	}
+   
+	$query =$query ." order by updatetime desc limit ".($num)*$pagesize.",$pagesize";
+	
+	//echo $query;
+	//return;
+	$json_arr = array();
+    $result = @$conn->query($query);
+    if ((!$result) ||($result->num_rows ==0))
+	{
+		//echo "<meta http-equiv='Content-Type'' content='text/html; charset=utf-8'>";
+      //  echo  $noconnect;
+	    echo json_encode( $json_arr );
+		return;
+    }
+    //$result = db_result_to_array($result);
+	//echo json_encode($result);
+	  $temp_arr = array();
+	 while($row = $result->fetch_assoc()){
+		 $temp_arr[] = $row;
+	 }
+
+	
+	 foreach($temp_arr as $k=>$v){
+		 $json_arr[]  = $v;
+
+	 }
+	 echo json_encode( $json_arr );
+
+
+?>
---
+++ waterfall.js
@@ -0,0 +1,95 @@
+﻿/*
+ *  Javascript文件：waterfall.js
+ */
+$(function(){
+	
+	/*alert("jareaselect:" + jareaselect);
+	alert("jstuffselect:" + jstuffselect);
+	alert("productlen:" + productlen);
+	alert("productwide:" + productwide);
+	alert("thinckness:" + thinckness);
+	alert("timber:" + timber);*/
+     jsonajax();
+ });
+ 
+ //这里就要进行计算滚动条当前所在的位置了。如果滚动条离最底部还有100px的时候就要进行调用ajax加载数据
+ $(window).scroll(function(){    
+     //此方法是在滚动条滚动时发生的函数
+     // 当滚动到最底部以上100像素时，加载新内容
+     var $doc_height,$s_top,$now_height;
+     $doc_height = $(document).height();        //这里是document的整个高度
+     $s_top = $(this).scrollTop();            //当前滚动条离最顶上多少高度
+     $now_height = $(this).height();            //这里的this 也是就是window对象
+     if(($doc_height - $s_top - $now_height) < 100) jsonajax();    
+ });
+ 
+ 
+ //做一个ajax方法来请求data.php不断的获取数据
+ var $num = 0;
+ function jsonajax(){
+	var jareaselect=0,jstuffselect=0,productlen=0,productwide=0,thinckness=0,diameterlen=0,timber=0,jkindselect=0;
+
+    jareaselect =document.getElementById("areaselect").value;
+    jstuffselect = document.getElementById("stuffselect").value;
+	jkindselect =document.getElementById("kindselect").value;
+	timber=document.getElementById("timber").value;
+	
+	if(document.getElementById("productlen").value.length == 0){
+		productlen=0;
+	}else{
+		productlen=document.getElementById("productlen").value;
+	}
+		
+	if(document.getElementById("productwide").value.length == 0){
+		productwide=0;
+	}else{
+		productwide=document.getElementById("productwide").value;
+	}
+		
+	if(document.getElementById("thinckness").value.length == 0){
+		thinckness=0;
+	}else{
+		thinckness=document.getElementById("thinckness").value;
+	}	
+
+	if(document.getElementById("diameterlen").value.length == 0){
+		diameterlen=0;
+	}else{
+		diameterlen=document.getElementById("diameterlen").value;
+	}
+
+     $.ajax({
+         url:'getdatalist.php',
+         type:'POST',
+         data:"num="+($num++)+"&areaselect="+jareaselect+"&kindselect="+jkindselect+"&stuffselect="+jstuffselect
+	  +"&productlen="+productlen+"&productwide="+productwide+"&thinckness="+thinckness+"&diameterlen="+diameterlen
+	  +"&timber="+timber,
+		/*"num="+$num+"&type=1&areaselect="+jareaselect+"&kindselect="+jkindselect+"&stuffselect="+jstuffselect
+	  +"&productlen="+productlen+"&productwide="+productwide+"&thinckness="+thinckness+"&diameterlen="+diameterlen
+	  +"&timber="+timber*/
+
+         dataType:'json',
+         success:function(json){
+             if(typeof json == 'object'){
+                 var neirou,$row,iheight,$item,$bianhua;
+                 for(var i=0;i<json.length;i++){
+                     neirou = json[i];    //当前层数据
+
+					 $row =$("#showdata1 ul");
+                     $item = $(
+						"<li class='list-item'>"+
+							"<a href='detail.php?productid="+neirou.productid+"' class='clearfix'>"+
+								"<div class='list-left'><span>"+neirou.carnum+"  "+neirou.productlen+"米"+neirou.stuffname+"   "+neirou.wide+"*"+neirou.thinckness+"   "+neirou.portname+"   "+neirou.updatetime+"</span></div>"+
+								"<div class='list-right'><i class='icon-chevron-right'></i></div>"+
+							"</a>"+
+						"</li>"
+					 
+					 );	
+                     $row.empty().append($item);
+                     $item.fadeIn();
+                 }
+
+            }
+         }
+     });
+ }
